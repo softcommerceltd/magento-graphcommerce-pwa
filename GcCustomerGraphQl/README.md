ac90634b50a6e6569c734fbc3857a8aff38493c1
theexten <serhiy@softcommerce.co.uk>
2023-10-18 20:03:42 +0100

    first commit

---
+++ GcCustomerGraphQl/README.md
@@ -0,0 +1,9 @@
+## About
+Magento - GraphCommerce Customer GraphQl module.
+
+## Purpose
+Used to extend functionality of `Magento_CustomerGraphQl` module.
+
+## Support
+Soft Commerce Ltd <br />
+support@softcommerce.co.uk
---
+++ GcCustomerGraphQl/registration.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Copyright © Soft Commerce Ltd. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'SoftCommerce_GcCustomerGraphQl',
+    __DIR__
+);
